50bbba9c66c7d79711fd41f07f2a99b060aaa6a7
Liz Magalindan <emagalindan@pinterest.com>
2018-09-24 12:01:59 -0700

    Clean up GitBlameInfoModal

--- webapp/src/main/resources/public/js/components/workbench/GitBlameInfoModal.js
+++ webapp/src/main/resources/public/js/components/workbench/GitBlameInfoModal.js
@@ -8,13 +8,17 @@ let TextUnitsGitBlameModal = React.createClass({
 
     propTypes() {
         return {
-            "isShowGitBlameModal": PropTypes.bool.isRequired
+            "isShowGitBlameModal": PropTypes.bool.isRequired,
+            "repoIds": PropTypes.array,
+            "repositories": PropTypes.array
         };
     },
 
     getDefaultProps() {
         return {
-            "isShowGitBlameModal": false
+            "isShowGitBlameModal": false,
+            "repoIds": [],
+            "repositories": []
         };
     },
 
@@ -25,36 +29,11 @@ let TextUnitsGitBlameModal = React.createClass({
      *  }}
      */
     getInitialState() {
-        this.REVIEW = "review";
-        this.REJECT = "reject";
-        this.ACCEPT = "accept";
-        this.TRANSLATE = "translate";
+
         return {
-            "currentReviewState": this.getInitialReviewStateOfTextUnits(),
-            "comment": this.getInitialTargetCommentOfTextUnits()
+            "repoIds": this.props.repoIds,
+            "repositories": this.props.repositories
         };
-    },
-
-    /**
-     * Sets the state of the component to the button that was clicked upon.
-     * @param {string} reviewState
-     */
-    optionClicked(reviewState) {
-        this.setState({
-            "currentReviewState": reviewState
-        });
-    },
-
-    /**
-     * Creates the data object containing comments and chosen action option and calls the
-     * parent action handler to perform the action on the textunit.
-     */
-    onReviewModalSaveClicked() {
-        let modalData = {
-            "comment": this.state.comment,
-            "textUnitAction": this.state.currentReviewState
-        };
-        this.props.onReviewModalSaveClicked(modalData);
     },
 
     /**
@@ -65,137 +44,6 @@ let TextUnitsGitBlameModal = React.createClass({
             isShowModal: false
         });
         this.props.onCloseModal();
-    },
-
-    /**
-     * @returns {JSX} The JSX for the reject button with class active set according to the current component state
-     */
-    getRejectButton() {
-        return (
-            <Button active={this.state.currentReviewState === this.REJECT}
-                    onClick={this.optionClicked.bind(this, this.REJECT)}>
-
-                <FormattedMessage id="textUnit.reviewModal.rejected"/>
-            </Button>
-        );
-    },
-
-    /**
-     * @returns {JSX} The JSX for the review button with class active set according to the current component state
-     */
-    getReviewButton() {
-        return (
-            <Button active={this.state.currentReviewState === this.REVIEW}
-                    onClick={this.optionClicked.bind(this, this.REVIEW)}>
-
-                <FormattedMessage id="textUnit.reviewModal.needsReview"/>
-            </Button>
-        );
-    },
-
-    /**
-     * @returns {JSX} The JSX for the accept button with class active set according to the current component state
-     */
-    getAcceptButton() {
-        return (
-            <Button active={this.state.currentReviewState === this.ACCEPT}
-                    onClick={this.optionClicked.bind(this, this.ACCEPT)}>
-
-                <FormattedMessage id="textUnit.reviewModal.accepted"/>
-            </Button>
-        );
-    },
-
-    /**
-     * @returns {JSX} The JSX for the translate button with class active set according to the current component state
-     */
-    getTranslateButton() {
-        return (
-            <Button active={this.state.currentReviewState === this.TRANSLATE}
-                    onClick={this.optionClicked.bind(this, this.TRANSLATE)}>
-
-                <FormattedMessage id="textUnit.reviewModal.translationNeeded"/>
-            </Button>
-        );
-    },
-
-    /**
-     * @returns {string} The current review state of the selected textunits (reject/review/accept)
-     * if ALL textunits have the same state, empty string otherwise.
-     */
-    getInitialReviewStateOfTextUnits() {
-        let selectedTextUnits = this.props.textUnitsArray;
-        let currentReviewState = "";
-        if (selectedTextUnits.length > 0) {
-            currentReviewState = this.getReviewStateOfTextUnit(selectedTextUnits[0]);
-        }
-        for (let index = 1; index < selectedTextUnits.length; index++) {
-            let textUnitReviewState = this.getReviewStateOfTextUnit(selectedTextUnits[index]);
-            if (textUnitReviewState !== currentReviewState) {
-                currentReviewState = "";
-                break;
-            }
-        }
-        return currentReviewState;
-    },
-
-    /**
-     * @returns {string} The target comment of the selected textunits if ALL textunits have
-     * the same comment, empty string otherwise.
-     */
-    getInitialTargetCommentOfTextUnits() {
-        let selectedTextUnits = this.props.textUnitsArray;
-        let initialTargetComment = "";
-        if (selectedTextUnits.length > 0) {
-            initialTargetComment = this.getTargetCommentOfTextUnit(selectedTextUnits[0]);
-        }
-        for (let index = 1; index < selectedTextUnits.length; index++) {
-            let textUnitTargetComment = this.getTargetCommentOfTextUnit(selectedTextUnits[index]);
-            if (textUnitTargetComment !== initialTargetComment) {
-                initialTargetComment = "";
-                break;
-            }
-        }
-        return initialTargetComment;
-    },
-
-    /**
-     * @param {TextUnit} textUnit
-     * @returns {string} The current review state of the textUnit passed in.
-     */
-    getReviewStateOfTextUnit(textUnit) {
-        let currentReviewState = "";
-        if (typeof textUnit !== "undefined") {
-            currentReviewState = this.ACCEPT;
-            if (!textUnit.isIncludedInLocalizedFile()) {
-                currentReviewState = this.REJECT;
-            } else if (textUnit.getStatus() === TextUnit.STATUS.REVIEW_NEEDED) {
-                currentReviewState = this.REVIEW;
-            } else if (textUnit.getStatus() === TextUnit.STATUS.TRANSLATION_NEEDED) {
-                currentReviewState = this.TRANSLATE;
-            }
-
-        }
-        return currentReviewState;
-    },
-
-    /**
-     * @param {TextUnit} textUnit
-     * @returns {string} The target comment of the textunit passed in.
-     */
-    getTargetCommentOfTextUnit(textUnit) {
-        let targetComment = "";
-        if (typeof textUnit !== "undefined") {
-            targetComment = textUnit.getTargetComment();
-        }
-        return targetComment;
-    },
-
-    /**
-     * @param {Event} event
-     */
-    commentTextAreaOnChange(event) {
-        this.setState({"comment": event.target.value});
     },
 
     render() {
